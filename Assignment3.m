32b9c4c505feb3e791b0243b2e5ffa763716194e
John King III <king.2047@osu.edu>
2013-09-26 17:39:53 -0400

    Create Assignment3.m

---
+++ Assignment3.m
@@ -0,0 +1,25 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Assignment 3 - A Vast Array of String Puns, None of Them Very Good 
+%
+%  ________________________________
+%/ Today's lucky MD5 hash:          \
+%\ cd500413e7aed6e29a4da6f59113fd7f /
+%  --------------------------------
+%         \   ^__^ 
+%          \  (oo)\_______
+%             (__)\       )\/\
+%                 ||----w |
+%                 ||     ||
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clc
+clear
+format compact
+disp('King, John')
+disp('ENGR1181, Seat #38 (Table J)')
+disp('Schrock, 5:20PM')
+disp('Assignment 3')
+
+disp('Problem 14')
+vecA=(49:-3:10)
+vec
